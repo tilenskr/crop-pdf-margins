3bce4e4cb917a25ccc75e2e2379d2ccf9f38a240
tilenskr <skrinjar.tilen@gmail.com>
2025-07-22 22:26:05 +0200

    Refactor annotation extraction into separate function

--- src/crop/scale_cropper/annotations.py
+++ src/crop/scale_cropper/annotations.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import warnings
 import pymupdf
 
@@ -16,64 +17,89 @@ def copy_annotations(src: pymupdf.Document, dst: pymupdf.Document):
 
         for src_annotation in src_page.annots():
             # r = src_annotation.rect
-            annotation_type = src_annotation.type[0]
+            annotation_type = AnnotType(src_annotation.type[0])
 
-            dst_annotation: pymupdf.Annot
             try:
-                match annotation_type:
-                    case AnnotType.PDF_ANNOT_CARET:
-                        dst_annotation = dst_page.add_caret_annot(src_annotation.rect.tl)
-                    case AnnotType.PDF_ANNOT_TEXT:
-                        dst_annotation = dst_page.add_text_annot(
-                            src_annotation.rect.tl,
-                            src_annotation.info["content"],
-                            src_annotation.info["name"],
-                        )
-                    case AnnotType.PDF_ANNOT_FILE_ATTACHMENT:
-                        try:
-                            src_filename = src_annotation.file_info["filename"]
-                        except Exception:
-                            src_filename = "attachment"
-                        dst_annotation = dst_page.add_file_annot(
-                            src_annotation.rect.tl,
-                            src_annotation.get_file(),
-                            src_filename,
-                            src_annotation.file_info.get("ufilename", src_filename),
-                            src_annotation.file_info["description"],
-                            src_annotation.info["name"],
-                        )
-                    case AnnotType.PDF_ANNOT_FREE_TEXT:
-                        free_text_info = extract_font_info(src, src_annotation)
-                        dst_annotation = dst_page.add_freetext_annot(
-                            src_annotation.rect,
-                            free_text_info.text,
-                            
-                            fontsize=free_text_info.font_size,
-                            fontname=free_text_info.font_name,
-                            text_color=free_text_info.text_color,
-                            fill_color=free_text_info.fill_color,
-                            border_color=free_text_info.border_color,
-                            border_width=free_text_info.border_width,
-                            dashes=free_text_info.dashes,
-                            callout=free_text_info.callout,
-                            line_end=free_text_info.line_end,
-                            opacity=free_text_info.opacity,
-                            align=free_text_info.align,
-                            rotate=free_text_info.rotate,
-                            richtext=free_text_info.richtext,
-                        )
-
-                    case _:
-                        # should print a warning or log here
-                        warnings.warn(
-                            f"Unsupported annotation type {AnnotType(annotation_type).name} on page {page_num + 1}. Skipping."
-                        )
-                        continue
+                dst_annotation = get_annotation(src, src_annotation, annotation_type, dst_page)
+                if not dst_annotation:
+                    warnings.warn(f"Unsupported annotation type ({annotation_type.name}) on page {page_num + 1}. Skipping.")
+                    continue
             except Exception as e:
-                warnings.warn(f"Error copying annotation ({AnnotType(annotation_type).name}) on page {page_num + 1}: {e}")
+                warnings.warn(f"Error copying annotation ({annotation_type.name}) on page {page_num + 1}: {e}")
                 continue
 
-            # elif a_type == PDF_ANNOT_LINE:
+            xref_map[src_annotation.xref] = dst_annotation.xref
+
+            dst_annotation.set_info(src_annotation.info)
+            dst_annotation.set_border(src_annotation.border)
+            if src_annotation.blendmode is not None:
+                dst_annotation.set_blendmode(src_annotation.blendmode)
+            if annotation_type != AnnotType.PDF_ANNOT_FREE_TEXT:
+                dst_annotation.set_colors(src_annotation.colors)
+            dst_annotation.set_flags(src_annotation.flags)
+            if src_annotation.irt_xref != 0:
+                dst_annotation.set_irt_xref(xref_map[src_annotation.irt_xref])
+            if src_annotation.line_ends is not None:
+                dst_annotation.set_line_ends(src_annotation.line_ends[0], src_annotation.line_ends[1])
+            dst_annotation.set_name(src_annotation.info["name"])
+            dst_annotation.set_oc(src_annotation.get_oc())
+            dst_annotation.set_opacity(src_annotation.opacity)
+            dst_annotation.set_open(src_annotation.is_open)
+            dst_annotation.set_popup(src_annotation.popup_rect)
+            dst_annotation.set_rect(src_annotation.rect)
+            dst_annotation.set_rotation(src_annotation.rotation)
+            dst_annotation.update()
+
+def get_annotation(src_document: pymupdf.Document, src_annotation: pymupdf.Annot, 
+                   annotation_type: AnnotType, dst_page: pymupdf.Page) -> Optional[pymupdf.Annot]:
+    match annotation_type:
+        case AnnotType.PDF_ANNOT_CARET:
+            return dst_page.add_caret_annot(src_annotation.rect.tl)
+        case AnnotType.PDF_ANNOT_TEXT:
+            return dst_page.add_text_annot(
+                src_annotation.rect.tl,
+                src_annotation.info["content"],
+                src_annotation.info["name"],
+            )
+        case AnnotType.PDF_ANNOT_FILE_ATTACHMENT:
+            try:
+                src_filename = src_annotation.file_info["filename"]
+            except Exception:
+                src_filename = "attachment"
+            return dst_page.add_file_annot(
+                src_annotation.rect.tl,
+                src_annotation.get_file(),
+                src_filename,
+                src_annotation.file_info.get("ufilename", src_filename),
+                src_annotation.file_info["description"],
+                src_annotation.info["name"],
+            )
+        case AnnotType.PDF_ANNOT_FREE_TEXT:
+            free_text_info = extract_font_info(src_document, src_annotation)
+            return dst_page.add_freetext_annot(
+                src_annotation.rect,
+                free_text_info.text,
+                
+                fontsize=free_text_info.font_size,
+                fontname=free_text_info.font_name,
+                text_color=free_text_info.text_color,
+                fill_color=free_text_info.fill_color,
+                border_color=free_text_info.border_color,
+                border_width=free_text_info.border_width,
+                dashes=free_text_info.dashes,
+                callout=free_text_info.callout,
+                line_end=free_text_info.line_end,
+                opacity=free_text_info.opacity,
+                align=free_text_info.align,
+                rotate=free_text_info.rotate,
+                richtext=free_text_info.richtext,
+            )
+
+        case _:
+            return None
+        
+
+           # elif a_type == PDF_ANNOT_LINE:
             #     # add_line_annot(p1, p2)
             #     p1, p2 = verts[0], verts[-1] if verts else (r.tl, r.br)
             #     dst_annotation = dst_page.add_line_annot(p1, p2)
@@ -158,26 +184,3 @@ def copy_annotations(src: pymupdf.Document, dst: pymupdf.Document):
             # elif a_type == PDF_ANNOT_WIDGET:
             #     # form‐field widgets are complex; skip or handle separately
             #     continue
-
-              
-            xref_map[src_annotation.xref] = dst_annotation.xref
-
-            dst_annotation.set_info(src_annotation.info)
-            dst_annotation.set_border(src_annotation.border)
-            if src_annotation.blendmode is not None:
-                dst_annotation.set_blendmode(src_annotation.blendmode)
-            if annotation_type != AnnotType.PDF_ANNOT_FREE_TEXT:
-                dst_annotation.set_colors(src_annotation.colors)
-            dst_annotation.set_flags(src_annotation.flags)
-            if src_annotation.irt_xref != 0:
-                dst_annotation.set_irt_xref(xref_map[src_annotation.irt_xref])
-            if src_annotation.line_ends is not None:
-                dst_annotation.set_line_ends(src_annotation.line_ends[0], src_annotation.line_ends[1])
-            dst_annotation.set_name(src_annotation.info["name"])
-            dst_annotation.set_oc(src_annotation.get_oc())
-            dst_annotation.set_opacity(src_annotation.opacity)
-            dst_annotation.set_open(src_annotation.is_open)
-            dst_annotation.set_popup(src_annotation.popup_rect)
-            dst_annotation.set_rect(src_annotation.rect)
-            dst_annotation.set_rotation(src_annotation.rotation)
-            dst_annotation.update()
